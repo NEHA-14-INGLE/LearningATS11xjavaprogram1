f9063137aeaccf48f92238889c9e954986a92eb8
Neha ingle <ingleneha152@gmail.com>
2025-03-12 19:48:25 +0530

    table of 9

---
+++ src/Lab_UsingprintF.java
@@ -0,0 +1,9 @@
+
+public class Lab_UsingprintF {
+    public static void main(String[] args) {
+        int table = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d × %d = %d\n", table, i, table * i);
+        }
+    }
+}
